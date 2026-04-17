8f5836e5a0de5c68ddff4ee7a0c67ccec6697e3b
Phantomzzz7 <raresanton14@gmail.com>
2026-04-17 22:31:01 +0300

    Initialize VS Project

---
+++ First-Application/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace First_Application
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
